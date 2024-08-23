95195811529e42441a8b5f68c480a66d25d80358
golden-parrot <naveenbharathi5050@gmail.com>
2024-08-23 05:38:37 +0530

    footer | update

--- rules_and_regulations.php
+++ rules_and_regulations.php
@@ -11,6 +11,7 @@
     <link rel="shortcut icon" href="favicon.ico">
     <link rel="stylesheet" href="https://use.typekit.net/ftj8drh.css">
     <link rel="stylesheet" type="text/css" href="css/ps.css" />
+    <link rel="stylesheet" href="css/footersec.css">
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
     <link rel="stylesheet" href="https://use.typekit.net/ftj8drh.css">
@@ -329,66 +330,7 @@ footer {
     </div>
 
     
-    
-<footer style="padding-top: 30px;">
-    <div class="container">
-        <div class="row">
-            <!-- Follow Us Section -->
-            <div class="col-md-6">
-                <div class="follow-box">
-                    <h3>Follow Us</h3>
-                    <ul class="follow-box-copyright">
-                        <li>
-                            <a href="https://www.facebook.com/nscetofficial" target="_blank" rel="noopener noreferrer">
-
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                    <path fill="#3b5998" d="M22 12c0-5.522-4.477-10-10-10s-10 4.478-10 10c0 4.991 3.657 9.128 8.438 9.878v-6.988h-2.54v-2.89h2.54v-2.19c0-2.505 1.492-3.89 3.777-3.89 1.095 0 2.24.195 2.24.195v2.461h-1.262c-1.243 0-1.63.774-1.63 1.565v1.859h2.773l-.443 2.89h-2.33v6.988c4.781-.75 8.438-4.887 8.438-9.878z"/>
-                                </svg>
-                                
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://x.com/nscettheni?s=20" target="_blank">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                    <path fill="#1da1f2" d="M24 4.557a9.9 9.9 0 01-2.828.775 4.938 4.938 0 002.165-2.723 9.865 9.865 0 01-3.127 1.195 4.924 4.924 0 00-8.384 4.49c-4.086-.204-7.702-2.165-10.126-5.144a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.231-.616v.061a4.924 4.924 0 003.946 4.827 4.935 4.935 0 01-2.224.085 4.927 4.927 0 004.599 3.417 9.867 9.867 0 01-6.1 2.103c-.396 0-.787-.023-1.175-.069a13.963 13.963 0 007.547 2.212c9.056 0 14.002-7.498 14.002-13.986 0-.213-.005-.425-.014-.637a10.013 10.013 0 002.457-2.548l.002-.002z"/>
-                                </svg>
-                                
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://www.youtube.com/channel/UCDquPXiTrHksqrsF66G0cQQ" target="_blank">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                    <path fill="#FF0000" d="M23.498 6.186a2.972 2.972 0 00-2.094-2.096C19.859 3.596 12 3.596 12 3.596s-7.86 0-9.404.494a2.972 2.972 0 00-2.094 2.096c-.496 1.547-.496 4.774-.496 4.774s0 3.227.496 4.774a2.972 2.972 0 002.094 2.096c1.544.494 9.404.494 9.404.494s7.859 0 9.404-.494a2.972 2.972 0 002.094-2.096c.496-1.547.496-4.774.496-4.774s0-3.227-.496-4.774zM9.997 15.569v-6.164l6.421 3.083-6.421 3.081z"/>
-                                </svg>
-                                
-                            </a>
-                        </li>
-                    </ul>
-                    
-                    <p style="margin-top: 15px;">© 2024-25 NSCET  Hackathon. All rights reserved</p>
-                </div>
-            </div>
-
-            <!-- Contact Us Section -->
-            <div class="col-md-6">
-                <div class="follow-box flo-box">
-                    <h3>Contact Us</h3>
-                    <ul class="follow-box-contact">
-                        <li>
-                            <i class="fa fa-phone" aria-hidden="true"></i>
-                            <a href="tel:+91 4546 263900">+91 4546 263900</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-envelope"></i>
-                            <a href="mailto:principal@nscet.org">principal@nscet.org</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
-
+  <?php include("./resources/footersec.php") ?>
     <!-- End of PS Section -->
     <script src="js/gsap.min.js"></script>
     <!-- <script type="module" src="js/index.js"></script> -->
