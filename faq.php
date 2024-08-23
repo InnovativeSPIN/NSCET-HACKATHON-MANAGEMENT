8561ac3583ff62f2b1a4c5e0935375427b2ae5c3
santhosh <sandhoshtechie02@gmail.com>
2024-08-23 12:28:36 +0530

    faq page added

---
+++ faq.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/nav.css">
+    <link rel="stylesheet" href="./css/mobilenav.css">
+    <link rel="stylesheet" href="/css/footersec.css">   
+</head>
+<body>
+
+<?php include("./resources/header.php") ?>
+
+
+
+<?php include("./resources/footersec.php") ?>
+    
+</body>
+</html>
--- resources/connection.php
+++ resources/connection.php
@@ -2,16 +2,16 @@
     session_start();
 
     // Comment this in Testing
-    // $db_host = "localhost";
-    // $db_user = "root";
-    // $db_pass = "";
-    // $db_name = "nscet_hackathon";
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "nscet_hackathon";
 
     // Uncomment this in production
-    $db_host = "localhost";
-    $db_user = "nscet_webadmin";
-    $db_pass = "x}2^7Wk^H^.n";
-    $db_name = "nscet_hackathon";
+    // $db_host = "localhost";
+    // $db_user = "nscet_webadmin";
+    // $db_pass = "x}2^7Wk^H^.n";
+    // $db_name = "nscet_hackathon";
 
     $conn = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
     if(mysqli_connect_error())
