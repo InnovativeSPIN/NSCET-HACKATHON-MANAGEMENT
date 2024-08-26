309f4116e5977790483441d0936ace48e5ecf7d4
santhosh <sandhoshtechie02@gmail.com>
2024-08-26 10:11:59 +0530

    reg bg animation change

--- register.php
+++ register.php
@@ -27,7 +27,8 @@
     }
 
     body {
-      background-image: url('/img/loginbg.jpg');
+      background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);
+
       background-size: cover;
       background-position: center;
       background-repeat: no-repeat;
@@ -35,6 +36,7 @@
       margin: 0;
       padding: 0;
     }
+    
 
     .frame__title {
       z-index: 1;
@@ -58,7 +60,11 @@
 <body>
 
   <?php include("./resources/header.php") ?>
-  <canvas id="test"></canvas>
+  <div class="animation-bg">
+<div id='stars'></div>
+    <div id='stars2'></div>
+    <div id='stars3'></div>
+</div>
   <div class="main-body">
     <div class="container">
       <div class="inner-content">
@@ -172,95 +178,7 @@
           }
       });
   </script>
-  <script>
-
-    var w = window.innerWidth,
-      h = window.innerHeight,
-      canvas = document.getElementById('test'),
-      ctx = canvas.getContext('2d'),
-      rate = 60,
-      arc = 100,
-      time,
-      count,
-      size = 20,
-      speed = 15,
-      parts = new Array,
-      colors = ['yellow', '#ce5c00', '#5c3566'];
-    var mouse = { x: 0, y: 0 };
-
-    canvas.setAttribute('width', w);
-    canvas.setAttribute('height', h);
-
-    function create() {
-      time = 0;
-      count = 0;
-
-      for (var i = 0; i < arc; i++) {
-        parts[i] = {
-          x: Math.ceil(Math.random() * w),
-          y: Math.ceil(Math.random() * h),
-          toX: Math.random() * 5 - 1,
-          toY: Math.random() * 2 - 1,
-          c: colors[Math.floor(Math.random() * colors.length)],
-          size: Math.random() * size
-        }
-      }
-    }
-
-    function particles() {
-      ctx.clearRect(0, 0, w, h);
-      canvas.addEventListener('mousemove', MouseMove, false);
-      for (var i = 0; i < arc; i++) {
-        var li = parts[i];
-        var distanceFactor = DistanceBetween(mouse, parts[i]);
-        var distanceFactor = Math.max(Math.min(15 - (distanceFactor / 20), 5), 1);
-        ctx.beginPath();
-        ctx.arc(li.x, li.y, li.size * distanceFactor, 0, Math.PI * 2, false);
-        ctx.fillStyle = li.c;
-        ctx.strokeStyle = li.c;
-        if (i % 2 == 0)
-          ctx.stroke();
-        else
-          ctx.fill();
-
-        li.x = li.x + li.toX * (time * 0.05);
-        li.y = li.y + li.toY * (time * 0.05);
-
-        if (li.x > w) {
-          li.x = 0;
-        }
-        if (li.y > h) {
-          li.y = 0;
-        }
-        if (li.x < 0) {
-          li.x = w;
-        }
-        if (li.y < 0) {
-          li.y = h;
-        }
-
-
-      }
-      if (time < speed) {
-        time++;
-      }
-      setTimeout(particles, 1000 / rate);
-    }
-    function MouseMove(e) {
-      mouse.x = e.layerX;
-      mouse.y = e.layerY;
-
-      //context.fillRect(e.layerX, e.layerY, 5, 5);
-      //Draw( e.layerX, e.layerY );
-    }
-    function DistanceBetween(p1, p2) {
-      var dx = p2.x - p1.x;
-      var dy = p2.y - p1.y;
-      return Math.sqrt(dx * dx + dy * dy);
-    }
-    create();
-    particles();
-  </script>
+  
 </body>
 
 </html>
